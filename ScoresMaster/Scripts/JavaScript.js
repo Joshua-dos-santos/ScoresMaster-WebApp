99eb01e5862c18e526ce2b4ca176ec57a16b3383
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-02-25 16:06:19 +0100

    API Request
    Getting Matchdata from api

--- ScoresMaster/Scripts/JavaScript.js
+++ ScoresMaster/Scripts/JavaScript.js
@@ -1,6 +1,6 @@
 ﻿jQuery.getJSON('https://app.sportdataapi.com/api/v1/soccer/matches?apikey=7ccb81a0-7049-11eb-8c76-a5e6c19519e0&season_id=352&date_from=2021-02-24', function (result) {
     console.log(result)
-    var Get_Results1 = result.data[0]
+    var Get_Results1 = result.data[1]
     var start_time = Get_Results1.match_start
     var away = Get_Results1.away_team.name
     var home = Get_Results1.home_team.name

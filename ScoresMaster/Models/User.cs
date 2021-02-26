8303ba1dc7a135dbd5f5e1dff6395190d4c8bc67
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-02-26 16:17:25 +0100

    Javascript into cshtml file
    Put javascript into cshtml file for further progress

---
+++ ScoresMaster/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoresMaster.Models
+{
+    public class User
+    {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}

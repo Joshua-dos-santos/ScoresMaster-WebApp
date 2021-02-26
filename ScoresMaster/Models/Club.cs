2e5528b5ddcb19acfab169b15e6b174dd3c71d0e
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-02-26 22:07:49 +0100

    First setup classes
    properties aan de eerste paar klassen gegeven

--- ScoresMaster/Models/Club.cs
+++ ScoresMaster/Models/Club.cs
@@ -7,5 +7,8 @@ namespace ScoresMaster.Models
 {
     public class Club
     {
+        public string Name { get; set; }
+        public string League { get; set; }
+        public DateTime Year_of_Origin { get; set; }
     }
 }
--- ScoresMaster/Models/Player.cs
+++ ScoresMaster/Models/Player.cs
@@ -7,5 +7,12 @@ namespace ScoresMaster.Models
 {
     public class Player
     {
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public int Shirt_Number { get; set; }
+        public string Position { get; set; }
+        public string Nationality { get; set; }
+        public string Club { get; set; }
+        public DateTime Birth_Day { get; set; } 
     }
 }

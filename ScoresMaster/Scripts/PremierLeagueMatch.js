1a9f581bb2de248b2b963fe732756525825d6b47
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-02-26 13:41:29 +0100

    Commit tussendoor
    een commit tussendoor

--- ScoresMaster/Scripts/PremierLeagueMatch.js
+++ ScoresMaster/Scripts/PremierLeagueMatch.js
@@ -1,4 +1,4 @@
-﻿jQuery.getJSON('https://app.sportdataapi.com/api/v1/soccer/matches?apikey=7ccb81a0-7049-11eb-8c76-a5e6c19519e0&season_id=352&date_from=2021-02-24', function (result) {
+﻿jQuery.getJSON('https://app.sportdataapi.com/api/v1/soccer/matches?apikey=78b7f4f0-781d-11eb-b7ce-ab513a4a050f&season_id=352&date_from=2021-02-24', function (result) {
     console.log(result)
     var Get_Results1 = result.data[1]
     var start_time = Get_Results1.match_start

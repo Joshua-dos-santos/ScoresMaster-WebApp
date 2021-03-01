d473d704420a2c30dc4b48aafc15ba936db6dcbd
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-03-01 11:11:43 +0100

    Database Properties
    Made a database class and gave the database class properties

---
+++ ScoresMaster/Models/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScoresMaster.Models
+{
+    public class Database
+    {
+        private static string dbIp = "127.0.0.1";
+        private static string dbPort = "3306";
+        private static string dbUname = "root";
+        private static string dbPassword = "";
+        private static string dbName = "gogreens";
+        private static string pAPI_key = "";
+        private static string dbConnectionString = "Datasource=" + dbIp + ";port=" + dbPort + ";username=" + dbUname + ";password=;database=" + dbName + ";";
+
+        public static string DbIp { get { return dbIp; } }
+        public static string DbPort { get { return dbPort; } }
+        public static string DbUname { get { return dbUname; } }
+        public static string DbPassword { get { return dbPassword; } }
+        public static string DbName { get { return dbName; } }
+        public static string PAPI_Key
+        {
+            get { return pAPI_key; }
+            set { pAPI_key = value; }
+        }
+        public static string DbConnectionString
+        {
+            get { return dbConnectionString; }
+            set { dbConnectionString = value; }
+        }
+    }
+}

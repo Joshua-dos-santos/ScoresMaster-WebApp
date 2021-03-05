2944ecfb2a31b913b95140e5235982f5b7818dd3
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-03-05 10:40:00 +0100

    Loginscherm & registratiescherm
    Login en registratiescherm aangemaakt

--- ScoresMaster/Models/RegistrationModel.cs
+++ ScoresMaster/Models/RegistrationModel.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
 namespace ScoresMaster.Models
 {
-    public class User
+    public class RegistrationModel
     {
+        [DisplayName("First Name")]
         public string First_Name { get; set; }
         public string Last_Name { get; set; }
-        public string Email { get; set; }
+        public string EMail { get; set; }
         public string Password { get; set; }
+        public DateTime Birth_Day { get; set; }
     }
 }

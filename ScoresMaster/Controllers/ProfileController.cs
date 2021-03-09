dba1c1784bba2eb4c095509983701f0ceb2e6a67
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-03-09 13:59:17 +0100

    Myprofile creation
    begin gemaakt aan MyProfile en zorgen dat daar de dropdownlist komt te staan

--- ScoresMaster/Controllers/ProfileController.cs
+++ ScoresMaster/Controllers/ProfileController.cs
@@ -10,8 +10,15 @@ namespace ScoresMaster.Controllers
     public class ProfileController : Controller
     {
         // GET: Profile
-        public ActionResult Profile()
+        public ActionResult MyProfile()
         {
+            LeagueDatabase leagueDatabase = new LeagueDatabase();
+            List<string> Leagues = new List<string>();
+            for (int i = 1; i <= 33; i++)
+            {
+                Leagues.Add(leagueDatabase.GetLeagues(i));
+            }
+            ViewBag.League = Leagues;
             return View("MyProfile"); 
         }
     }

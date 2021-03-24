10f85d8875e7949a50437e21a9d380d2c4976f1d
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-03-24 13:01:07 +0100

    API werkend via 1 method

--- ScoresMaster/Api.cs
+++ ScoresMaster/Api.cs
@@ -29,21 +29,21 @@ namespace ScoresMaster
             return result;
         }
 
-        public static string GetSerieAMatches()
-        {
-            string result = null;
-            string requestString = "https://app.sportdataapi.com/api/v1/soccer/matches?apikey=78b7f4f0-781d-11eb-b7ce-ab513a4a050f&season_id=619&date_from=2021-03-25";
-            WebRequest requestObject = WebRequest.Create(requestString);
-            requestObject.Method = "GET";
-            HttpWebResponse responseObject = null;
-            responseObject = (HttpWebResponse)requestObject.GetResponse();
-            using (Stream stream = responseObject.GetResponseStream())
-            {
-                StreamReader sr = new StreamReader(stream);
-                result = sr.ReadToEnd();
-                sr.Close();
-            }
-            return result;
-        }
+        //public static string GetSerieAMatches()
+        //{
+        //    string result = null;
+        //    string requestString = "https://app.sportdataapi.com/api/v1/soccer/matches?apikey=78b7f4f0-781d-11eb-b7ce-ab513a4a050f&season_id=619&date_from=2021-03-25";
+        //    WebRequest requestObject = WebRequest.Create(requestString);
+        //    requestObject.Method = "GET";
+        //    HttpWebResponse responseObject = null;
+        //    responseObject = (HttpWebResponse)requestObject.GetResponse();
+        //    using (Stream stream = responseObject.GetResponseStream())
+        //    {
+        //        StreamReader sr = new StreamReader(stream);
+        //        result = sr.ReadToEnd();
+        //        sr.Close();
+        //    }
+        //    return result;
+        //}
     }
 }

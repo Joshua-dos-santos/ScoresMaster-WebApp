efc5596d55fdc1eb77c344a1d98a3a590dd74240
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-04-07 13:39:12 +0200

    circustrein update

--- Algoritmiek/CircusTrein/CircusTrein/Form1.cs
+++ Algoritmiek/CircusTrein/CircusTrein/Form1.cs
@@ -13,15 +13,16 @@ namespace CircusTrein
     public partial class Form1 : Form
     {
         List<Dier> AlleDieren = new List<Dier>();
+        Dier dier = new Dier();
         public Form1()
         {
             InitializeComponent();
-            
+
         }
 
         private void DierToevoegenBtn_Click(object sender, EventArgs e)
         {
-            Dier dier = new Dier();
+
             dier.DierType = DierNaamTB.Text;
             if (KleinRB.Checked == true)
             {
@@ -63,7 +64,45 @@ namespace CircusTrein
                 AlleDieren.Add(dier);
             }
         }
-
-        
+        public void SorteerDieren()
+        {
+            for (int i = 0; i <= AlleDieren.Count; i++)
+            {
+                if(dier.Voeding == VleesRB.Text && dier.Grootte == KleinRB.Text)
+                {
+                    if(Wagon1.Items.Contains(dier.Voeding == PlantenRB.Text && dier.Grootte == MiddelgrootRB.Text || dier.Grootte == GrootRB.Text))
+                    {
+                        Wagon1.Items.Add(dier);
+                    }
+                    else if(Wagon2.Items.Contains(dier.Voeding == PlantenRB.Text && dier.Grootte == MiddelgrootRB.Text || dier.Grootte == GrootRB.Text))
+                    {
+                        Wagon2.Items.Add(dier);
+                    }
+                    else
+                    {
+                        Wagon3.Items.Add(dier);
+                    }
+                }
+                else if(dier.Voeding == VleesRB.Text && dier.Grootte == MiddelgrootRB.Text)
+                {
+                    if(Wagon1.Items.Contains(dier.Voeding == PlantenRB.Text && dier.Grootte == GrootRB.Text) && !Wagon1.Items.Contains(dier.Voeding == VleesRB.Text && dier.Grootte == KleinRB.Text || dier.Voeding == VleesRB.Text && dier.Grootte == MiddelgrootRB.Text))
+                    {
+                        Wagon1.Items.Add(dier);
+                    }
+                    else if(Wagon2.Items.Contains(dier.Voeding == PlantenRB.Text && dier.Grootte == GrootRB.Text))
+                    {
+                        Wagon2.Items.Add(dier);
+                    }
+                    else
+                    {
+                        Wagon3.Items.Add(dier);
+                    }
+                }
+                else if(dier.Voeding == VleesRB.Text && dier.Grootte == GrootRB.Text)
+                {
+                    
+                }
+            }
+        }
     }
 }

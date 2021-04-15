a02c51ce02be3d106ee8fc88ae5bf6aa928c97cb
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-04-15 13:31:34 +0200

    CircusTrein Klasses aangemaakt

---
+++ Algoritmiek/CircusTrein/CircusTrein/Dier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CircusTrein
+{
+    class Dier
+    {
+        public enum VoedingTypen
+        {
+            Carnivore,
+            Herbivore
+        }
+
+
+    }
+}

79b8a2dc85e9bbdee2c7a10616f921b5763dd9ae
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-04-15 13:49:42 +0200

    Dieren Toevoegen

--- Algoritmiek/CircusTrein/CircusTrein/Dier.cs
+++ Algoritmiek/CircusTrein/CircusTrein/Dier.cs
@@ -6,12 +6,29 @@ namespace CircusTrein
 {
     class Dier
     {
+        public VoedingTypen Voeding { get; set; }
+        public Grootte GrootteDier { get; set; }
+
+
         public enum VoedingTypen
         {
-            Carnivore,
-            Herbivore
+            Vleeseter,
+            PlantenEter
         }
 
-
+        public enum Grootte
+        {
+            Klein = 1,
+            Middelgroot = 3,
+            Groot = 5
+        }
+        public List<Dier> Voeg_Dieren()
+        {
+            Dier Leeuw = new Dier { GrootteDier = Grootte.Groot, Voeding = VoedingTypen.Vleeseter };
+            Dier Muis = new Dier { GrootteDier = Grootte.Klein, Voeding = VoedingTypen.PlantenEter };
+            Dier Aap = new Dier { GrootteDier = Grootte.Middelgroot, Voeding = VoedingTypen.PlantenEter };
+            List<Dier> AlleDieren = new List<Dier> { Leeuw, Muis, Aap };
+            return AlleDieren;
+        }
     }
 }

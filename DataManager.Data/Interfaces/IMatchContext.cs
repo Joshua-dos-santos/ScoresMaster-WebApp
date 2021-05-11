434537ddffb857bce45d41eec012819399c8a851
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-05-11 14:27:54 +0200

    Match Added
    - added MatchDTO
    - added MatchRepo
    - added MatchAPIContext
    -added Match interface

---
+++ DataManager.Data/Interfaces/IMatchContext.cs
@@ -0,0 +1,14 @@
+﻿using DataManager.Data.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Data.Interfaces
+{
+    interface IMatchContext
+    {
+        IEnumerable<MatchDTO> GetAllMatches(int id, MatchDTO matchDTO);
+    }
+}
---
+++ DataManager.Data/Repositories/MatchRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataManager.Data.DTO;
+using DataManager.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Data.Repositories
+{
+    class MatchRepository
+    {
+        private IMatchContext context;
+
+        public List<MatchDTO>GetAllMatches(int id, MatchDTO matchDTO)
+        {
+            return context.GetAllMatches(id, matchDTO).ToList();
+        }
+    }
+}

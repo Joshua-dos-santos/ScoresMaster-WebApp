21aebc81b8c9811a8dcbb2d65be7a2291900dddb
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-05-12 11:50:31 +0200

    Added Business layer

---
+++ DataManager.Business/DTO/PositionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataManager.Data.DTO
+{
+    class PositionDTO
+    {
+        public int unique_id { get; set; }
+        public string Positions { get; set; }
+    }
+}

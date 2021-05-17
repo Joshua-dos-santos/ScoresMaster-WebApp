2d699ffeb7b536d8698b777069e8d5c01ecd997b
Joshua-dos-santos <74915903+Joshua-dos-santos@users.noreply.github.com>
2021-05-17 09:12:12 +0200

    MatchDAO toegevoegd

--- DataManager.Business/DAO/MatchDAO.cs
+++ DataManager.Business/DAO/MatchDAO.cs
@@ -9,5 +9,17 @@ namespace DataManager.Business.DAO
 {
     class MatchDAO
     {
+        public int MatchID { get; set; }
+        public string Home_Team { get; set; }
+        public string Match_Start { get; set; }
+        public string Away_Team { get; set; }
+
+        public MatchDAO(MatchDTO matchDTO)
+        {
+            MatchID = matchDTO.MatchID;
+            Home_Team = matchDTO.Home_Team;
+            Match_Start = matchDTO.Match_Start;
+            Away_Team = matchDTO.Away_Team;
+        }
     }
 }
